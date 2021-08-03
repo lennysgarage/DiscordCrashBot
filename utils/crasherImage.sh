8aac6ca33aee2a3ddd2cea29280bf03c142702e6
lennysgarage <lennysgarage1@gmail.com>
2021-08-03 19:25:41 -0400

    Fixed up crasher.sh and added image version

---
+++ utils/crasherImage.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Compiling video $1 with crasher to output video $2"
+ffmpeg -i $1 "crashers/newvideo2.mp4" # Converting image to video
+echo "file crashers/newvideo2.mp4\nfile crashers/newvideo.mp4" > crasher.txt
+ffmpeg -f concat -safe 0 -y -i "crasher.txt" -codec copy $2 # Allows me to overwrite 
+rm crasher.txt
+rm crashers/newvideo2.mp4
+echo "Goodbye."
+
