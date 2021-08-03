b2d2c851fc5d222a7819355812a07387f03bcebc
lennysgarage <lennysgarage1@gmail.com>
2021-08-03 18:46:48 -0400

    Increased timeout to allow for larger videos

--- commands/crasher.js
+++ commands/crasher.js
@@ -24,7 +24,7 @@ module.exports = {
                             })
                             .then()
                             .catch(console.error);
-                        }, 2000);
+                        }, 3000);
 
                     
                     
