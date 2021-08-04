1410533d821e6c3b79b30e59a26809cf44961bdf
lennysgarage <lennysgarage1@gmail.com>
2021-08-03 21:54:50 -0400

    Check for different file types

--- commands/crasher.js
+++ commands/crasher.js
@@ -1,4 +1,4 @@
-const { download } = require('../utils/download');
+const { downloadImage, downloadVideo } = require('../utils/download');
 
 
 module.exports = {
@@ -7,29 +7,37 @@ module.exports = {
     execute(message, args) {
         message.channel.messages.fetch({limit: 2})
             .then(msgs => {
-                if (msgs.last().attachments) {
-                   // if (msgs.last().attachments.first().name === `mp4`){
-                    // Want to check file type before hand
-                    download(msgs.last().attachments.first().url); 
-
-                    // Im not a timeout because I can't get my code to properly wait for the bash script to finish
-                    // Im not sure if i'm doing callbacks wrong
-                    // I've tried much with no avail trying callback's and promises
-                   setTimeout(function(){ 
-                        message.channel.send({
-                            files: [{
-                                attachment: 'crashers/crashy.mp4',
-                                name: 'coolfile.mp4'
-                                }]
-                            })
-                            .then()
-                            .catch(console.error);
-                        }, 3000);
-
+            if (msgs.last().attachments) {
+                let attachedFile = msgs.last().attachments.first().url;
+                if (attachIs(attachedFile, "png") || attachIs(attachedFile, "jpeg") || attachIs(attachedFile, "jpg")) {
+                    downloadImage(attachedFile); 
+                }
+                if (attachIs(attachedFile, "mp4")) {
+                    downloadVideo(attachedFile);
+                }
                     
-                    
-            }
+
+                // Im not a timeout because I can't get my code to properly wait for the bash script to finish
+                // Im not sure if i'm doing callbacks wrong
+                // I've tried much with no avail trying callback's and promises
+                setTimeout(function(){ 
+                    message.channel.send({
+                        files: [{
+                            attachment: 'crashers/crashy.mp4',
+                            name: 'coolfile.mp4'
+                            }]
+                        })
+                        .then()
+                        .catch(console.error);
+                    }, 3000);
+                }
         })
         .catch(console.error);
     }
+}
+
+
+function attachIs(url, fileFormat) {
+    //True if this url has fileFormat format.
+    return url.indexOf(fileFormat, url.length - fileFormat.length /*or 3*/) !== -1;
 }
