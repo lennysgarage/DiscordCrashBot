6cb375400b28ce856df4522f9cd33897e313bca9
lennysgarage <lennysgarage1@gmail.com>
2021-08-03 21:55:38 -0400

    Allow for mp4s and images to be crashing mp4s

--- utils/crasherImage.sh
+++ utils/crasherImage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Compiling video $1 with crasher to output video $2"
-ffmpeg -i $1 "crashers/newvideo2.mp4" # Converting image to video
+ffmpeg -i $1 -pix_fmt yuv444p -f mp4 "crashers/newvideo2.mp4" # Converting image to video
 echo "file crashers/newvideo2.mp4\nfile crashers/newvideo.mp4" > crasher.txt
 ffmpeg -f concat -safe 0 -y -i "crasher.txt" -codec copy $2 # Allows me to overwrite 
 rm crasher.txt
--- utils/download.js
+++ utils/download.js
@@ -4,12 +4,26 @@ const { exec } = require('child_process');
 
 
 module.exports = {
-    download(url) {
+    downloadVideo(url) {
         const file = fs.createWriteStream("downloads/testy.mp4");
         const request = http.get(url, (response) => {
             response.pipe(file)
             .on('finish', () => {
                 exec("sh utils/crasher.sh 'downloads/testy.mp4' 'crashers/crashy.mp4'", (err, stdout, stderr) => {
+                    console.log(stdout);
+                    console.error(stderr);
+                    console.error(err);
+                }); 
+            });
+        });
+    },
+
+    downloadImage(url) {
+        const file = fs.createWriteStream("downloads/testy.png");
+        const request = http.get(url, (response) => {
+            response.pipe(file)
+            .on('finish', () => {
+                exec("sh utils/crasherImage.sh 'downloads/testy.png' 'crashers/crashy.mp4'", (err, stdout, stderr) => {
                     console.log(stdout);
                     console.error(stderr);
                     console.error(err);
